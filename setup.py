83945775ab3a724ba71120616d0157e639a8f37b
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2013-04-05 00:17:46 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
         name='gfpy',
         version='0.0.1',
-        py_modules=['pygf'],
+        py_modules=['gfpy'],
         requires=['requests', 'pyyaml'],
 )
