473aa500a19f07564f1421ea185d55235e137dfd
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2013-04-05 19:02:55 +0900

    Use pytest

--- tests/test_gf.py
+++ tests/test_gf.py
@@ -1,51 +1,52 @@
 # -*- coding: utf-8 -*-
 from pygf import *
-import unittest
+import pygf
 
 
-class TestGrowthForecast(unittest.TestCase):
+class TestGrowthForecast(object):
 
-    def setUp(self):
-        self.gf = GrowthForecast('192.168.1.22')
-        for graph in self.gf.all():
-            self.gf.delete(graph)
-        self.assertEqual(len(self.gf), 0)
+    def pytest_funcarg__gf(self):
+        gf = GrowthForecast('192.168.1.22')
+        for graph in gf.all():
+            gf.delete(graph)
+        assert len(gf) == 0
+        return gf
 
-    def test_post(self):
-        graph = self.gf.post('foo', 'bar', 'baz', 1)
-        self.assertIsInstance(graph, Graph)
-        self.assertEqual(len(self.gf), 1)
+    def test_post(self, gf):
+        graph = gf.post('foo', 'bar', 'baz', 1)
+        assert isinstance(graph, Graph)
+        assert len(gf) == 1
 
-        self.gf.delete(graph)
-        self.assertEqual(len(self.gf), 0)
+        gf.delete(graph)
+        assert len(gf) == 0
 
-    def test_iter(self):
-        graph1 = self.gf.post('foo', 'bar', 'baz1', 1)
-        graph2 = self.gf.post('foo', 'bar', 'baz2', 1)
+    def test_iter(self, gf):
+        graph1 = gf.post('foo', 'bar', 'baz1', 1)
+        graph2 = gf.post('foo', 'bar', 'baz2', 1)
 
-        self.assertEqual(len(self.gf), 2)
-        self.assertIsInstance(self.gf.all(), list)
-        for graph in self.gf.all():
-            self.assertIsInstance(graph, Graph)
+        assert len(gf) == 2
+        assert isinstance(gf.all(), list)
+        for graph in gf.all():
+            assert isinstance(graph, Graph)
 
-        self.assertEqual(self.gf.tree().keys(), ['foo'])
-        self.assertEqual(self.gf.tree()['foo'].keys(), ['bar'])
-        self.assertEqual(self.gf.tree()['foo']['bar'].keys(), ['baz1', 'baz2'])
+        assert gf.tree().keys() == ['foo']
+        assert gf.tree()['foo'].keys() == ['bar']
+        assert gf.tree()['foo']['bar'].keys() == ['baz1', 'baz2']
 
-        graph = self.gf.by_name('foo', 'bar', 'baz1')
-        self.assertIsInstance(graph, Graph)
-        self.assertEqual(graph['graph_name'], 'baz1')
+        graph = gf.by_name('foo', 'bar', 'baz1')
+        assert isinstance(graph, Graph)
+        assert graph['graph_name'] == 'baz1'
 
-    def test_add_graph(self):
-        graph = self.gf.add_graph('foo', 'bar', 'graph1', initial_value=0, color='#223344', mode='gauge')
-        self.assertIsInstance(graph, Graph)
-        self.assertEqual(graph['graph_name'], 'graph1')
+    def test_add_graph(self, gf):
+        graph = gf.add_graph('foo', 'bar', 'graph1', initial_value=0, color='#223344', mode='gauge')
+        assert isinstance(graph, Graph)
+        assert graph['graph_name'] == 'graph1'
 
-        graph = self.gf.add_graph('foo', 'bar', 'graph2', initial_value=0, color='#223344', mode='gauge')
-        self.assertIsInstance(graph, Graph)
-        self.assertEqual(graph['graph_name'], 'graph2')
+        graph = gf.add_graph('foo', 'bar', 'graph2', initial_value=0, color='#223344', mode='gauge')
+        assert isinstance(graph, Graph)
+        assert graph['graph_name'] == 'graph2'
 
-    def test_complex(self):
-        loc = self.gf.add_complex('foo', 'bar', 'complex1', 'tests', False, 19, 'AREA', 'gauge', False, [1, 2])
-        self.assertIsInstance(loc, basestring)
-        self.assertTrue('/list/foo/bar' in loc)
+    def test_complex(self, gf):
+        loc = gf.add_complex('foo', 'bar', 'complex1', 'tests', False, 19, 'AREA', 'gauge', False, [1, 2])
+        assert isinstance(loc, basestring)
+        assert '/list/foo/bar' in loc
